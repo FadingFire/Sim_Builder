d039a53d992370a65a842b79e045447a28f66e29
Wazeback <88798120+Wazeback@users.noreply.github.com>
2023-09-11 21:39:10 +0000

    small fix on left over code

--- python/src/main/endpoint/post_endpoints/command_line.py
+++ python/src/main/endpoint/post_endpoints/command_line.py
@@ -17,18 +17,12 @@ def HandleCommand(self):   # TODO: remember to talk to me about how you want to 
 def parseCommand(self):
     content_length = int(self.headers['Content-Length'])
     post_data = self.rfile.read(content_length).decode('utf-8')
-    for i, char in enumerate(post_data):
-        if(char == "="):
-            break
     validatedData = validataRawPostData(post_data)
     if(validatedData[0] != 200):
         return 505
-    
     command = post_data[validatedData[1]:len(post_data)]
     print(command)
-    
-    # TODO: this does work its just that some char still get converted becacause you cant have spaces inside of stuff etc 
-    
+    # TODO: this does work its just that some char still get converted becacause you cant have spaces inside of stuff etc
     if(validateCommand(self, command) != 200):
         return 505
     return 200, command
