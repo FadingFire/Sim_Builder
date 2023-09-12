ffd798dd26d8979feeb763faf818f9795b3e93f2
Wazeback <88798120+Wazeback@users.noreply.github.com>
2023-09-12 07:26:43 +0000

    added propper pyton variable function naming

--- python/src/main/endpoint/post_endpoints/command_line.py
+++ python/src/main/endpoint/post_endpoints/command_line.py
@@ -1,44 +1,50 @@
 from http.server import SimpleHTTPRequestHandler
 from model.paths import post_response_page
 
-# TODO: a thing you could want tot write is some code that uses the bluesky api to validate the responses though i dont if they have that programmed in.
-
-
-
-def HandleCommand(self):   # TODO: remember to talk to me about how you want to return a succes message etc
-    command = parseCommand(self)
-    if(command[0] != 200):
+def handle_command(self):
+    """the main order of when what function happens"""
+    command = parse_command(self)
+    if command[0] != 200:
         return 505
-    useCommand(command[1])
+    use_command(command[1])
     self.path = post_response_page()
     return SimpleHTTPRequestHandler.do_GET(self)
 
 
-def parseCommand(self):
+def parse_command(self):
+    """this parses the POST data to separate the input name from its value"""
     content_length = int(self.headers['Content-Length'])
     post_data = self.rfile.read(content_length).decode('utf-8')
-    validatedData = validataRawPostData(post_data)
-    if(validatedData[0] != 200):
+    validate_data = validata_raw_post_data(post_data)
+    if validate_data[0] != 200:
         return 505
-    command = post_data[validatedData[1]:len(post_data)]
+    command = post_data[validate_data[1]:len(post_data)]
     print(command)
-    # TODO: this does work its just that some char still get converted becacause you cant have spaces inside of stuff etc
-    if(validateCommand(self, command) != 200):
+    if validate_command(self, command) != 200:
         return 505
     return 200, command
 
 
-def validataRawPostData(postData):
-    for i, char in enumerate(postData):
-        if(char == "=" and i+1 <= len(postData) ):   # FIXME: this might actualy be the other way around instead of <= maybe its >= idk im stupid and not testing it
+def validata_raw_post_data(post_data):
+    """validates the post data to check if there is a value"""
+    for i, char in enumerate(post_data):
+        if char == "=" and i+1 <= len(post_data):
             return 200, i+1
     return 404, 0
 
 
-def validateCommand(self, command):     # TODO: check if the parsed data is a command that either the user can do or if the command actualy exist
+def validate_command(self, command):
+    """
+    check against all the possible commands that you can have in blueksy to check if it does exist.
+    run the command in a dummy blueksy terminal to see the response?
+    """
     return 200
 
 
-def useCommand(command):     # TODO: this is going to be the painfull procces The command to the plugin
+def use_command(command):
+    """
+    runs the command using a the bluesky plugin.
+    this will only run live and should maybe not be used to stop or start blueksy?
+    """
     print(command)
     return
