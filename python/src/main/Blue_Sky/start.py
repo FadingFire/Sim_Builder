1b4053020dbd44420aa5c91a9c76ad0b54d81c9b
FadingFire <martijnvaag2@gmail.com>
2023-09-13 15:31:45 +0200

    trying to spawn planes(not working)

--- python/src/main/Blue_Sky/start.py
+++ python/src/main/Blue_Sky/start.py
@@ -1,5 +1,24 @@
 from bluesky import __main__
+import bluesky as bs
 
 
-if __name__ == '__main__':
-    __main__.main()
+__main__.main()
+
+
+# initialize bluesky as non-networked simulation node
+bs.init(mode='sim', detached=True)
+
+
+n = 3
+
+
+bs.traf.mcre(n, actype="B744")
+
+
+for acid in bs.traf.id:
+    bs.stack.stack(f'ORIG {acid} EGLL;'
+                   f'ADDWPT {acid} BPK FL60;'
+                   f'ADDWPT {acid} TOTRI FL107;'
+                   f'ADDWPT {acid} MATCH FL115;'
+                   f'ADDWPT {acid} BRAIN FL164;'
+                   f'VNAV {acid} ON')
