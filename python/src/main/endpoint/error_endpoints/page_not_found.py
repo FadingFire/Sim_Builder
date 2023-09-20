1b0d03a6c8a53cb5ca5b0fa4d94b605ec7f239fe
Wazeback <88798120+Wazeback@users.noreply.github.com>
2023-09-20 07:05:35 +0000

    deleted everything for reset

--- python/src/main/endpoint/error_endpoints/page_not_found.py
+++
@@ -1,6 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from python.src.main.model.paths import get_page_path
-
-def HandlePageNotFound(self):
-    self.path = get_page_path('404')
-    return SimpleHTTPRequestHandler.do_GET(self)
--- python/src/main/endpoint/get_endpoints/command_input.py
+++
@@ -1,7 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from python.src.main.model.paths import get_page_path
-
-def command_input_page(self):
-    self.path = get_page_path("commandInputPage")
-    print(self.path)
-    return SimpleHTTPRequestHandler.do_GET(self)
