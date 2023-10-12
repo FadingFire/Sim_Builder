a4ede194a93cbcf0e18d295bc0689571efcf6097
FadingFire <martijnvaag2@gmail.com>
2023-10-12 15:52:40 +0200

    added a few comments to the code

--- src/main/bluesky/creator.py
+++ src/main/bluesky/creator.py
@@ -2,11 +2,13 @@ from filePreparer import filepreparer
 from merger import process_and_save_data
 from parser import getdata
 
+# temp file setup
 inputfile = "Data/Flights4.csv"
 outputfile = "Data/complete.xlsx"
 landingsfile = "Data/Landings4.csv"
 scenefile = "Data/scenefile.scn"
 
-preparedfile = filepreparer(inputfile, landingsfile)
-completefile = process_and_save_data(preparedfile[0], preparedfile[1], outputfile)
+# running functions with correct files
+flightsfile, landingsfile = filepreparer(inputfile, landingsfile)
+completefile = process_and_save_data(flightsfile, landingsfile, outputfile)
 getdata(completefile, scenefile)
--- src/main/bluesky/parser.py
+++ src/main/bluesky/parser.py
@@ -7,6 +7,7 @@ airport_info_dict = airport_data.set_index("# code")[["lat", "lon"]].to_dict(ori
 
 
 def get_airport_info(code, info_type):
+    # making the airport info into a dictionary
     airport_info = airport_info_dict.get(code)
     if airport_info:
         return airport_info.get(info_type, None)
@@ -33,9 +34,10 @@ def write_scene_file(filename, combined_df):
             scenetext = ""
             if has_required_data(row):
                 lat, lon = get_airport_info(row["DEST"], "lat"), get_airport_info(row["DEST"], "lon")
-                ehamLat, ehamLon = 52.309, 4.764  # Replace with actual values
+                ehamLat, ehamLon = 52.309, 4.764
                 stack = row.get("STACK", None)
-                if lat is not None and lon is not None:  # Check if lat and lon are valid
+                # check if stack is not empty and set stack and brng
+                if lat is not None and lon is not None:
                     if stack in ["RIVER", "SUGOL", "ARTIP"]:
                         lat, lon = {
                             "RIVER": (51.912, 4.132),
@@ -43,9 +45,11 @@ def write_scene_file(filename, combined_df):
                             "ARTIP": (52.511, 5.569)
                         }.get(stack, (lat, lon))
                         brng = calculate_bearing(lat, lon, ehamLat, ehamLon)
+                    # else brng is calculated from origin
                     else:
                         brng = calculate_bearing(ehamLat, ehamLon, lat, lon)
                     flight_altitude = int(float(row["RFL"]))
+                    # if origin is Amsterdam, set the origin to EHAM and delete at a distance of 30
                     if row['ADEP'] == 'EHAM':
                         scenetext = (
                             f"00:00:00.00>CRE {row['CALLSIGN']} {row['ICAO_ACTYPE']} {row['ADEP']} {brng} "
@@ -53,6 +57,7 @@ def write_scene_file(filename, combined_df):
                             f"00:00:00.00>DEST {row['CALLSIGN']} {row['DEST']}\n"
                             f"00:00:00.00>{row['CALLSIGN']} ATDIST {row['ADEP']} 30 DEL {row['CALLSIGN']}\n"
                         )
+                    # else if stack is not empty set origin as the correct stack and delete at Amsterdam
                     elif pd.notna(stack):
                         scenetext = (
                             f"00:00:00.00>CRE {row['CALLSIGN']} {row['ICAO_ACTYPE']} {stack} {brng} "
@@ -60,22 +65,23 @@ def write_scene_file(filename, combined_df):
                             f"00:00:00.00>DEST {row['CALLSIGN']} {row['DEST']}\n"
                             f"00:00:00.00>{row['CALLSIGN']} AT {row['DEST']} DO DEL {row['CALLSIGN']}\n"
                         )
+                    # write to the scenefile
                     file.write(scenetext)
 
 
 def getdata(input_file, output_file):
+    # read the complete Excel file and drop all duplicate Callsigns
     combined_df = pd.read_excel(input_file)
     combined_df.drop_duplicates(subset="CALLSIGN", keep="first", inplace=True)
 
     def get_dest_lat_lon(dest):
+        # get the lat and lon of the destinations from the dictionary
         airport_info = airport_info_dict.get(dest)
         if airport_info:
             return airport_info.get("lat", None), airport_info.get("lon", None)
         else:
-            # Provide default values (you can replace these with appropriate defaults)
             return None, None
 
     combined_df["DEST_LATITUDE"], combined_df["DEST_LONGITUDE"] = zip(*combined_df["DEST"].map(get_dest_lat_lon))
-
+    # run write function with correct files
     write_scene_file(output_file, combined_df)
-
