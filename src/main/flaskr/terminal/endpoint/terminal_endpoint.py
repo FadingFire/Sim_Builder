0ad9bcbb03afbbc7e92d3e6c6d43e2e675729994
FadingFire <martijnvaag2@gmail.com>
2023-10-31 15:06:23 +0100

    fixed stuff

--- src/main/flaskr/terminal/endpoint/terminal_endpoint.py
+++ src/main/flaskr/terminal/endpoint/terminal_endpoint.py
@@ -1,5 +1,4 @@
 from flask import Blueprint
-from src.main.bluesky.creator import airlineslist
 from src.main.bluesky.Client import TextClient
 
 terminal_endpoint = Blueprint('terminal_endpoint', __name__)
@@ -7,32 +6,23 @@ terminal_endpoint = Blueprint('terminal_endpoint', __name__)
 from src.main.flaskr.globals.model.response import response_with_request, text_response
 
 
-
+bsclient = TextClient()
+bsclient.connect(event_port=11000, stream_port=11001)
 
 
 @terminal_endpoint.route("/<command>")
 def parse_command(command):
     # get all the airlines
-    bsclient = TextClient()
-    bsclient.connect(event_port=11000, stream_port=11001)
-    airline = airlineslist()
+
     # send all airlines to FE
     res = text_response
     res.update({
-        "status": command,
-        "message": airline
+        "status": 200,
+        "message": "the command has been seen by the server"
     })
     # stack commands
     if bsclient is not None:
-        if command == "A":
-            stack = "CRE 11 12 12 12 12 12 12"
-            bsclient.stack(stack)
-        if command == "B":
-            stack = "CRE 12 12 12 12 12 12 12"
-            bsclient.stack(stack)
-        if command == "C":
-            stack = "QUIT"
-            bsclient.stack(stack)
+        bsclient.stack(command)
     return res
 
 
