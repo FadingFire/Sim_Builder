d1396435f8a876bdda7cb72a2874915f8f2e60ba
FadingFire <martijnvaag2@gmail.com>
2023-10-31 09:08:54 +0100

    send stack to bluesky

--- src/main/bluesky/run.py
+++ src/main/bluesky/run.py
@@ -1,10 +1,4 @@
 import sys
 from bluesky.__main__ import main
-from src.main.flaskr.terminal.endpoint.terminal_endpoint import parse_command
 
-def stop():
-    print("text")
-
-def start():
-    sys.exit(main())
-start()
+sys.exit(main())
--- src/main/flaskr/terminal/endpoint/terminal_endpoint.py
+++ src/main/flaskr/terminal/endpoint/terminal_endpoint.py
@@ -1,11 +1,25 @@
-from flask import Blueprint, request
+import sys
+
+from flask import Blueprint
 from markupsafe import escape
 from src.main.bluesky.creator import airlineslist
-
 
 terminal_endpoint = Blueprint('terminal_endpoint', __name__)
 
 from src.main.flaskr.globals.model.response import response_with_request, text_response
+from bluesky.network.client import Client
+
+
+class TextClient(Client):
+    def __init__(self):
+        super().__init__()
+
+    def stack(self, text):
+        self.send_event(b'STACK', text, b"*")
+
+
+bsclient = TextClient()
+bsclient.connect(event_port=11000, stream_port=11001)
 
 
 @terminal_endpoint.route("/<command>")
@@ -13,40 +27,13 @@ def parse_command(command):
     airline = airlineslist()
     res = text_response
     res.update({
-            "status": command,
-            "message": airline
-        })
-    if command == "B":
-        command = "CRE 12 12 12 12 12 12 12"
-        try:
-            from PyQt5.QtCore import Qt, QTimer
-            from PyQt5.QtWidgets import QTextEdit
-        except ImportError:
-            from PyQt6.QtCore import Qt, QTimer
-            from PyQt6.QtWidgets import QTextEdit
-
-        from bluesky.network.client import Client
-
-
-        echobox = None
-
-        class TextClient(Client):
-            def __init__(self):
-                super().__init__()
-                self.timer = QTimer()
-                self.timer.timeout.connect(self.update)
-                self.timer.start(20)
-
-            def stack(self, text):
-                ''' Stack function to send stack commands to BlueSky. '''
-                self.send_event(b'STACK', text)
-
-
-        # Create and start BlueSky client
-        bsclient = TextClient()
-        bsclient.connect(event_port=11000, stream_port=11001)
+        "status": command,
+        "message": airline
+    })
+    stack = "CRE 12 12 12 12 12 12 12"
+    if bsclient is not None:
+        bsclient.stack(stack)
     return res
-
 
 
 @terminal_endpoint.route("/start")
@@ -57,21 +44,22 @@ def start_command():
     """
     res = text_response
     res.update({
-            "status": 200,
-            "message": "the start command has been seen by the server",
-        })
+        "status": 200,
+        "message": "the start command has been seen by the server",
+    })
     return res
+
 
 @terminal_endpoint.route("/stop")
 def stop_command():
     """sends stop command to the server
-    
+
     Returns:
         text_response: [Int, String]
     """
     res = text_response
     res.update({
-            "status": 200,
-            "message": "the start command has been seen by the server",
-        })
+        "status": 200,
+        "message": "the start command has been seen by the server",
+    })
     return res
